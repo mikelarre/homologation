62e1e0f03fe8b2a9453e93014d4eb48f2b2b2386
mikelarre <mikelarregi@avanzosc.es>
2018-10-23 16:25:53 +0200

    [ADD] new module purchase_order_allowed_manufacturer_for_customer

---
+++ purchase_order_allowed_manufacturer_for_customer/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © Copyright 2018 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Product_supplierinfo_for_customer combined with "
+            "purchase_order_allowed_manufacturer",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "product_supplierinfo_for_customer",
+        "purchase_order_allowed_manufacturer",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "",
+    "summary": "",
+    "data": [
+         
+    ],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ purchase_order_allowed_manufacturer_for_customer/models/purchase_order.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from openerp import api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    @api.depends('only_allowed_products', 'partner_id')
+    def _compute_allowed(self):
+        product_obj = self.env['product.template']
+        for order in self:
+            if order.only_allowed_products and order.partner_id:
+                suppinfo = self.env['product.supplierinfo'].search(
+                    [('name', 'in', (order.partner_id.commercial_partner_id.id,
+                                     order.partner_id.id)),
+                     ('approved', '=', True)])
+                order.allowed_tmpl_ids = suppinfo.mapped(
+                    'product_tmpl_id').filtered(
+                        lambda x: x.purchase_ok)
+            else:
+                order.allowed_tmpl_ids = product_obj.search(
+                    [('purchase_ok', '=', True)])
+            order.allowed_product_ids =\
+                order.mapped(
+                    'allowed_tmpl_ids.product_variant_ids').filtered(
+                        lambda x: x.purchase_ok
+                        and any(x.supplier_ids.mapped("approved")))
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+
+    @api.multi
+    @api.depends("product_id.supplier_ids")
+    def _compute_manufacturers(self):
+        for record in self:
+            supplier_id = record.product_id.supplier_ids.filtered(
+                lambda x: x.name.id == record.partner_id.id)
+            record.manufacturer_ids = supplier_id.manufacturer_ids.filtered(
+                lambda x: x.approved)
